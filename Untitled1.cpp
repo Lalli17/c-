672ee3a738033faab4d2d96886d728efc4df62c6
Lalli17 <113790298+Lalli17@users.noreply.github.com>
2022-10-19 19:34:05 +0530

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+ int a,b;
+ cout<<"enter two integers "<<endl;
+ cin>>a>>b;
+ if(a==b)
+ cout<<"numbers are equal "<<endl;
+ else
+ cout<<"numbers are not equal "<<endl;
+ 
+}
